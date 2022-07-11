e05bd79a5d62ca4be107f4032954496281f8176e
Iskander Gaba <IskanderGaba@Eaton.com>
2022-07-11 15:13:46 +0200

    Added Karaf dependencies tree reconstruction

---
+++ src/main/java/com/eaton/maven/plugin/karaf/validation/KarafDependencyTreeMojo.java
@@ -0,0 +1,79 @@
+package com.eaton.maven.plugin.karaf.validation;
+
+import com.eaton.maven.plugin.karaf.validation.dependency.DependencyTreeHelper;
+import com.eaton.maven.plugin.karaf.validation.dependency.DependencyTreeHelperFactory;
+import org.apache.karaf.tooling.utils.MojoSupport;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.repository.RepositorySystem;
+import org.apache.maven.shared.filtering.MavenFileFilter;
+import org.apache.maven.shared.filtering.MavenResourcesFiltering;
+import org.codehaus.plexus.PlexusContainer;
+import org.eclipse.aether.graph.DependencyNode;
+
+import java.util.List;
+
+
+@Mojo(name = "generate-dependencies-tree", defaultPhase = LifecyclePhase.VERIFY, aggregator = true, threadSafe = true)
+class KarafDependencyTreeMojo extends MojoSupport {
+
+    /**
+     * Maximum size of the artifact LRU cache. This cache is used to prevent repeated artifact-to-file resolution.
+     */
+    @Parameter(defaultValue = "1024")
+    private int artifactCacheSize;
+
+    /**
+     * Flag indicating whether transitive dependencies should be included (<code>true</code>) or not (<code>false</code>).
+     * <p/>
+     * N.B. Note the default value of this is true, but is suboptimal in cases where specific <code>&lt;feature/&gt;</code> dependencies are
+     * provided by the <code>inputFile</code> parameter.
+     */
+    @Parameter(defaultValue = "true")
+    private boolean includeTransitiveDependency;
+
+    /**
+     * Contains the full list of projects in the reactor.
+     */
+    @Parameter(defaultValue = "${reactorProjects}", readonly = true, required = true)
+    private List<MavenProject> reactorProjects;
+
+    @Component
+    private PlexusContainer container;
+
+    @Component
+    private RepositorySystem repoSystem;
+
+    @Component
+    protected MavenResourcesFiltering mavenResourcesFiltering;
+
+    @Component
+    protected MavenFileFilter mavenFileFilter;
+
+    @Component
+    private ProjectBuilder mavenProjectBuilder;
+
+    protected DependencyTreeHelper dependencyHelper;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        //String output = "";
+        for (MavenProject project : reactorProjects) {
+            this.dependencyHelper = DependencyTreeHelperFactory.createDependencyHelper(this.container, project,
+                    this.mavenSession, this.artifactCacheSize, getLog());
+            DependencyNode tree = this.dependencyHelper.getDependencyTree(project);
+            this.dependencyHelper.getDependencies(project, includeTransitiveDependency);
+
+            System.out.println(this.dependencyHelper.getTreeListing());
+        }
+
+
+    }
+
+}
---
+++ src/main/java/com/eaton/maven/plugin/karaf/validation/dependency/DependencyTreeHelperFactory.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.eaton.maven.plugin.karaf.validation.dependency;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
+
+import java.util.List;
+
+public class DependencyTreeHelperFactory {
+
+    DependencyTreeHelper dependencyHelper;
+
+    /**
+     * Create a new {@link DependencyTreeHelper} based on what has been found in
+     * {@link org.codehaus.plexus.PlexusContainer}.
+     *
+     * @param container    The Maven Plexus container to use.
+     * @param mavenProject The Maven project to use.
+     * @param mavenSession The Maven session.
+     * @param cacheSize    Size of the artifact/file LRU cache
+     * @param log          The log to use for the messages.
+     *
+     * @return The {@link DependencyTreeHelper} depending on the Maven version used.
+     *
+     * @throws MojoExecutionException If the plugin execution fails.
+     */
+    public static DependencyTreeHelper createDependencyHelper(
+            PlexusContainer container, MavenProject mavenProject, MavenSession mavenSession, int cacheSize, Log log
+    ) throws MojoExecutionException {
+        try {
+            final RepositorySystem system = container.lookup(RepositorySystem.class);
+            final RepositorySystemSession session = mavenSession.getRepositorySession();
+            final List<RemoteRepository> repositories = mavenProject.getRemoteProjectRepositories();
+            return new DependencyTreeHelper(repositories, session, system, cacheSize);
+        } catch (ComponentLookupException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+}
