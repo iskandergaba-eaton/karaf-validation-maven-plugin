43d820fa91fe1e7d9321d927c977a36f6ca91c87
Buchs, Thibaud <ThibaudBuchs@eaton.com>
2022-07-11 15:13:53 +0200

    add test dependency1 module

---
+++ src/main/java/com/eaton/maven/plugin/karaf/validation/dependency/conflict/Identifier.java
@@ -0,0 +1,42 @@
+package com.eaton.maven.plugin.karaf.validation.dependency.conflict;
+
+import java.util.Objects;
+
+public class Identifier {
+	
+	final String groupId;
+	
+	final String artifactId;
+
+	public Identifier(String groupId, String artifactId) {
+		super();
+		this.groupId = groupId;
+		this.artifactId = artifactId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(artifactId, groupId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Identifier other = (Identifier) obj;
+		return Objects.equals(artifactId, other.artifactId) && Objects.equals(groupId, other.groupId);
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+	
+}
